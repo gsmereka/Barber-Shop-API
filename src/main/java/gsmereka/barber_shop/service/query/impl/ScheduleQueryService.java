2c86bc50207c71a389f5eb4df5641809e4f1b46e
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-04-13 21:04:21 -0300

    feat: implement ScheduleQueryService.java and ClientQueryService.java

---
+++ src/main/java/gsmereka/barber_shop/service/query/impl/ScheduleQueryService.java
@@ -0,0 +1,38 @@
+package gsmereka.barber_shop.service.query.impl;
+
+import gsmereka.barber_shop.entity.ScheduleEntity;
+import gsmereka.barber_shop.repository.IScheduleRepository;
+import gsmereka.barber_shop.service.query.IScheduleQueryService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
+public class ScheduleQueryService implements IScheduleQueryService {
+
+    private final IScheduleRepository repository;
+
+    @Override
+    public ScheduleEntity findById(final long id) {
+        return repository.findById(id).orElseThrow(
+//                () -> new NotFoundException("Agendamento não encontrado")
+        );
+    }
+
+    @Override
+    public List<ScheduleEntity> findInMonth(final OffsetDateTime startAt, final OffsetDateTime endAt) {
+        return repository.findByStartAtGreaterThanEqualAndEndAtLessThanEqualOrderByStartAtAscEndAtAsc(startAt, endAt);
+    }
+
+    @Override
+    public void verifyIfScheduleExists(final OffsetDateTime startAt, final OffsetDateTime endAt) {
+        if (repository.existsByStartAtAndEndAt(startAt, endAt)){
+            var message = "Já existe um cliente agendado no horário solicitado";
+//            throw new ScheduleInUseException(message);
+        }
+    }
+
+}
